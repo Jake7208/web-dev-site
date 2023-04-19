89537c3924fc918bdf1b5c7c9a6bb79d6b0d5e5c
Jake7208 <92753785+Jake7208@users.noreply.github.com>
2023-04-19 15:23:37 -0600

    resources

--- server/api/routes/resources.js
+++ server/api/routes/resources.js
@@ -1,4 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', (req, res, next ) => { // getting the announcementRoutes key from the app.js file.
+    res.status(200).json({
+        message: 'Handling GET request to /resources'
+    });
+});
+
+router.post('/', (req, res, next ) => {
+    res.status(201).json({
+        message: 'Handling POST request to /resources',
+    });
+});
+
+
+router.get('/getById/:resourcesId', (req, res, next) => {
+        res.status(200).json({
+            message: 'newsLetter details',
+            newsLetterId: req.params.resourcesId
+        })
+})
+
+
+
+router.delete('/deleteById/:newsLetterId', (req, res, next) => {
+    const id = req.params.newsLetterId
+   if (id !== undefined) {
+        res.status(200).json({
+            message: `resources id: ${id}; deleted`,
+            newsLetterId: id
+        })
+    }
+})
 module.exports = router; // connecting to the router on the app.js file.

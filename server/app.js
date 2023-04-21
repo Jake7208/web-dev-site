5f37e7544941c5988580098cf9e8553988c1cc34
Jake7208 <92753785+Jake7208@users.noreply.github.com>
2023-04-20 23:23:19 -0600

    mongo db properly installed
    certain bugs come back and fix database go learn mongoose!

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {MongoClient} = require('mongodb')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')

27015b3d158b2fa8ec0670c34ee311271b4c461c
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-25 15:51:31 -0600

    testing announcements

--- app/src/components/AnnouncementBanner.js
+++ app/src/components/AnnouncementBanner.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AnnouncementBanner = () => {
-  return <div>AnnouncementBanner</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://test-vercel-56nw26r7g-jake7208.vercel.app/api/announcements/getAll"
+    );
+  });
+  return (
+    <div>
+      {posts.map((post) => {
+        return <div className="post-card" key={post.id}></div>;
+      })}
+    </div>
+  );
 };
 
 export default AnnouncementBanner;

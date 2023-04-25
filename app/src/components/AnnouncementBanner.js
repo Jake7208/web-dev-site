a22d259a6ad6f0aca599b42ac2f774eb2869fe50
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-25 16:00:53 -0600

    test announcement api

--- app/src/components/AnnouncementBanner.js
+++ app/src/components/AnnouncementBanner.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 
 const AnnouncementBanner = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     fetch(
-      "https://test-vercel-56nw26r7g-jake7208.vercel.app/api/announcements/getAll"
+      "https://test-vercel-56nw26r7g-jake7208.vercel.app/api/announcements/updateById/123456"
     );
   });
   return (
@@ -15,17 +14,6 @@ const AnnouncementBanner = () => {
       })}
     </div>
   );
-=======
-import React from "react";
-import "./AnnouncementBanner.css";
-
-const AnnouncementBanner = () => {
-	return (
-		<div className="banner">
-			<h1>Announcement Banner Goes Here</h1>
-		</div>
-	);
->>>>>>> 9461a776113fa8024e8a00f09af913229f32db60
 };
 
 export default AnnouncementBanner;

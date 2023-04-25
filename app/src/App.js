ed84db9fd8e112dad6bf0409e747ac4b009f20ab
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-25 14:11:48 -0600

    announcement banner

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import SideNavBar from "./components/SideNavBar";
+import AnnouncementBanner from "./components/AnnouncementBanner";
 
 function App() {
   return (
     <>
       <SideNavBar />
+      <AnnouncementBanner />
     </>
   );
 }
---
+++ app/src/components/AnnouncementBanner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnnouncementBanner = () => {
+  return (
+    <div>
+      <h1>Announcement Banner Goes Here</h1>
+    </div>
+  );
+};
+
+export default AnnouncementBanner;

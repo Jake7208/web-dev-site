a74f5b0ef03537bea7911fa50f8e65c4b7c6f7a9
dandy1ion <104789558+dandy1ion@users.noreply.github.com>
2023-04-25 14:34:32 -0600

    positioned banner

--- app/src/components/AnnouncementBanner.js
+++ app/src/components/AnnouncementBanner.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const AnnouncementBanner = () => {
-  return (
-    <div>
-      <h1>Announcement Banner Goes Here</h1>
-    </div>
-  );
+	return (
+		<div className="banner">
+			<h1>Announcement Banner Goes Here</h1>
+		</div>
+	);
 };
 
 export default AnnouncementBanner;

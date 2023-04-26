a844dd7d5d420062725c5a3246d66d975fe86717
dandy1ion <104789558+dandy1ion@users.noreply.github.com>
2023-04-25 18:14:00 -0600

    spacing

--- app/src/components/AnnouncementBanner.js
+++ app/src/components/AnnouncementBanner.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import "./AnnouncementBanner.css";
+
 const AnnouncementBanner = () => {
-  return (
-    <div>
-      <h1>Announcement Banner Goes Here</h1>
-    </div>
-  );
+	return (
+		<div className="banner">
+			<h1>Announcement Banner Goes Here</h1>
+		</div>
+	);
 };
 
 export default AnnouncementBanner;

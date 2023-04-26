48e99ccd3c24863c1522f08b9795742c818b7f5d
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-26 10:36:46 -0600

    hooks

--- app/src/components/AnnouncementBanner.js
+++ app/src/components/AnnouncementBanner.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./AnnouncementBanner.css";
 
 const AnnouncementBanner = () => {
-	return (
-		<div className="banner">
-			<h1>Announcement Banner Goes Here</h1>
-		</div>
-	);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://test-vercel-56nw26r7g-jake7208.vercel.app/api/announcements/getAll"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }, []);
+  return (
+    <div className="banner">
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <div>
+              <div>Delete</div>
+            </div>
+          </div>
+        );
+      })}
+      <h1>Announcement Banner Goes Here</h1>
+    </div>
+  );
 };
 
 export default AnnouncementBanner;

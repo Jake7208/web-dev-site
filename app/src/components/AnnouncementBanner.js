a7a5db59814c4a51089c3de3ef6a799eb66d68ef
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-26 12:18:20 -0600

    cleaning up the API

--- app/src/components/AnnouncementBanner.js
+++ app/src/components/AnnouncementBanner.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 import "./AnnouncementBanner.css";
+import useFetch from "../Hooks/useFetch";
 
 const AnnouncementBanner = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    fetch(
-      "https://test-vercel-56nw26r7g-jake7208.vercel.app/api/announcements/getAll"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setPosts(data);
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
-  }, []);
+  const { data: posts, error } = useFetch(
+    `${process.env.REACT_APP_BACKEND_URL}/announcements/getAll`
+  );
+
+  if (!posts && !error) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!Array.isArray(posts)) {
+    return <p>Got: {posts.toString()}; expected Array.</p>;
+  }
+
   return (
     <div className="banner">
       {posts.map((post) => {

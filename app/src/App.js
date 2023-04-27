067907a2b81b63aa3e2c955a3414e8aa4e232d1e
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-27 11:26:26 -0600

    Newsletter api

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,16 @@
 import SideNavBar from "./components/SideNavBar";
 import AnnouncementBanner from "./components/AnnouncementBanner";
 import "./App.css";
+import NewsLetter from "./components/NewsLetter";
 
 function App() {
-	return (
-		<div className="row">
-			<SideNavBar />
-			<AnnouncementBanner />
-		</div>
-	);
+  return (
+    <div className="row">
+      <SideNavBar />
+      <AnnouncementBanner />
+      <NewsLetter />
+    </div>
+  );
 }
 
 export default App;

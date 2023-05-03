1195e51ecf3132697991886ba16b337ca81d21bc
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-03 15:10:40 -0600

    created the components

---
+++ app/src/Pages/AllTabs/AnnouncementTab.js
@@ -0,0 +1,10 @@
+import React from "react";
+const AnnouncementTab = () => {
+  return (
+    <div className="FirstTab">
+      <p>First Tab!! Hurray!!</p>
+      {/* First tab content will go here */}
+    </div>
+  );
+};
+export default AnnouncementTab;
---
+++ app/src/Pages/AllTabs/EventTab.js
@@ -0,0 +1,10 @@
+import React from "react";
+const EventTab = () => {
+  return (
+    <div className="EventTab">
+      <p>Second Tab!! Hurray!!</p>
+      {/* Second  tab content will go here */}
+    </div>
+  );
+};
+export default EventTab;

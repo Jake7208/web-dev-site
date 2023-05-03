e965fd7db2501e9e2c9b64416e3c7e58741909c7
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-03 15:00:09 -0600

    created my first two tabs

--- app/src/Pages/AdminPage.js
+++ app/src/Pages/AdminPage.js
@@ -2,12 +2,20 @@ import React from "react";
 import SideNavBar from "../components/SideNavBar";
 
 const AdminPage = () => {
-	return (
-		<div className="container">
-			<SideNavBar />
-			<h1 className="admin">..............AdminPage..........</h1>
-		</div>
-	);
+  return (
+    <div className="container">
+      <SideNavBar />
+      <div className="Tabs">
+        {/* Tab nav */}
+        <ul className="nav">
+          <li>Tab 1</li>
+          <li>Tab 2</li>
+        </ul>
+        <div className="outlet">{/* content will be shown here */}</div>
+      </div>
+      <h1 className="admin">..............AdminPage..........</h1>
+    </div>
+  );
 };
 
 export default AdminPage;

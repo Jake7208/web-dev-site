7ffcc624a5eafbd98a13a04341477bca7b37aa2e
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-04 13:23:34 -0600

    Added the pathway

---
+++ app/src/Pages/ViewVideos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewVideos = () => {
+  return <div>ViewVideos</div>;
+};
+
+export default ViewVideos;
--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AdminPage from "./Pages/AdminPage";
+import ViewVideos from "./Pages/ViewVideos";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
   {
     path: "AdminPage",
     element: <AdminPage />,
+  },
+  {
+    path: "ViewVideos",
+    element: <ViewVideos />,
   },
 ]);
 

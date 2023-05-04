78921d75c19ce75c2815fe9f6e41350bb72cd297
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-04 13:41:21 -0600

    api added to video component

--- app/src/Pages/ViewVideos.js
+++ app/src/Pages/ViewVideos.js
@@ -1,7 +1,29 @@
 import React from "react";
+import useFetch from "../Hooks/useFetch";
 
 const ViewVideos = () => {
-  return <div>ViewVideos</div>;
+  const { data: postURL, error } = useFetch(
+    `${process.env.REACT_APP_BACKEND_URL}/videos/getAll`
+  );
+
+  console.log(postURL, error);
+
+  if (!postURL && !error) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <h3>Videos</h3>
+      <div>
+        <iframe src={postURL}></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default ViewVideos;

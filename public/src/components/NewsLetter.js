0a15d8f6ed6cc8744540666a20e4dad404081221
dandy1ion <104789558+dandy1ion@users.noreply.github.com>
2023-05-18 11:33:13 -0600

    cleaning up

--- public/src/components/NewsLetter.js
+++ public/src/components/NewsLetter.js
@@ -3,29 +3,27 @@ import classes from "./NewsLetter.module.css";
 import useFetch from "../Hooks/useFetch";
 
 const NewsLetter = () => {
-  const { data: newslettersArray, error } = useFetch(
-    `${process.env.REACT_APP_BACKEND_URL}/newsLetter/getLatest`
-  );
+	const { data: newslettersArray, error } = useFetch(
+		`${process.env.REACT_APP_BACKEND_URL}/newsLetter/getLatest`,
+	);
 
-  console.log(newslettersArray, error);
+	console.log(newslettersArray, error);
 
-  if (!newslettersArray && !error) {
-    return <p>Loading...</p>;
-  }
+	if (!newslettersArray && !error) {
+		return <p>Loading...</p>;
+	}
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
+	if (error) {
+		return <p>Error: {error.message}</p>;
+	}
 
-  return (
-    <div className={classes.newscontainer}>
-      <h3 className={classes.newsletter}>Newsletter</h3>
-      <div className={classes.viewport}>
-        <iframe src={newslettersArray[0].url} className={classes.news}></iframe>
-        <p>{newslettersArray[0].title}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes.newscontainer}>
+			<h3 className={classes.newsletter}>Newsletter</h3>
+			<iframe src={newslettersArray[0].url} className={classes.news}></iframe>
+			<p>{newslettersArray[0].title}</p>
+		</div>
+	);
 };
 
 export default NewsLetter;
